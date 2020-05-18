1f0afe98e8e50c739142d02cc62ef1449141f2c5
Anuraag Minocha <anuraag.minocha@gmail.com>
2020-05-18 15:41:06 +0530

    Create home activity

---
+++ app/src/main/java/com/android/contactsapp/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.android.contactsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
